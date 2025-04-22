c8151ce9b230e779fd4b07a27085683a424db3f0
preitinger <peter.reitinger@gmail.com>
2025-04-22 20:06:28 +0200

    rebase-and-merge-feature-branch.sh: add arg for project name

--- script/rebase-and-merge-feature-branch.sh
+++ script/rebase-and-merge-feature-branch.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-feature_branch="$1"
+project="$1"
+feature_branch="$2"
 
-if [ "$1" = "" ]
+if [ "${project}" = "" -o "${feature_branch}" = "" ]
 then
-    echo "usage: $0 <feature branch>"
+    echo "usage: $0 <project name> <feature branch>"
     exit 1
 fi
 
+
 git switch local &&
-cd $(dirname $0)/../../../pr-example-next-private-submodules_MAIN &&
+cd $(dirname $0)/../../../${project}_MAIN &&
 git rebase --onto common local "${feature_branch}" &&
 git switch common &&
 git merge --ff-only "${feature_branch}" &&
 git branch -d "${feature_branch}" &&
 git switch main &&
 git merge common -m 'merge common into main' &&
-cd ../pr-example-next-private-submodules &&
+cd ../${project} &&
 git switch local &&
 git merge common -m 'merge common into local' &&
 echo 'The End' ||
