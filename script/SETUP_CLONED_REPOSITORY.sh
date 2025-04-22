1c66b99aa4ac8a0acbb4bb99316f50a20468471c
preitinger <peter.reitinger@gmail.com>
2025-04-22 21:34:26 +0200

    add SETUP_CLONED_REPOSITORY.sh

---
+++ script/SETUP_CLONED_REPOSITORY.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script is for setting up a fresh clone of a repository.
+# Recommended to copy it into the root directory of the default branch 'main'.
+
+cd $(dirname "$0")
+project=$(basename ${PWD})
+echo "project ${project}"
+
+git switch local &&
+git submodule update --init --recursive &&
+
+npm install &&
+git worktree add "../${project}_MAIN" main &&
+cd "../${project}_MAIN" &&
+npm install &&
+cd - &&
+
+
+
+echo 'The End.'
