1f4d76693e32351279f9c53b010219f0950fed7a
preitinger <peter.reitinger@gmail.com>
2025-04-23 08:28:46 +0200

    Skripte unabhaengig von Position der Skripte im Projekt gemacht.

--- script/common-submodule-add.sh
+++ script/common-submodule-add.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-submodule="$1"
+workspace="$1"
+submodule="$2"
 
 if [ "${submodule}" = "" ]
 then
-    echo "usage: $0 <repository to add as submodule in app/_lib/submodules>"
+    echo "usage: $0 <path to project root> <repository to add as submodule in app/_lib/submodules>"
     exit 1
 fi
 
-cd "$(dirname "$0")/../.." &&
+cd "${workspace}" &&
+ls app && echo 'workspace enthält Verzeichnis app; fahre fort...' || ( echo 'workspace enthält kein Verzeichnis app; bitte Argument 1 überprüfen.' && false ) &&
 workspace=${PWD} &&
 mkdir -p "${workspace}/app/_lib/submodules" &&
 cd "${workspace}/app/_lib/submodules" &&
@@ -16,7 +18,8 @@ pwd &&
 git submodule add --force ${submodule} &&
 git commit -m "SUBMODULE ADD ${submodule}" &&
 
-echo 'The End.'
+echo 'The End.' ||
+echo 'ERROR'
 
 # TODO Copy ${workspace}/app/_lib/submodules folder without .git files to ${workspace}/../${workspace}_MAIN/app/_lib/
 # Then, commit -m 'Copied content of submodules'
--- script/rebase-and-merge-feature-branch.sh
+++ script/rebase-and-merge-feature-branch.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-project="$1"
+workspace="$1"
 feature_branch="$2"
 
-if [ "${project}" = "" -o "${feature_branch}" = "" ]
+if [ "${workspace}" = "" -o "${feature_branch}" = "" ]
 then
-    echo "usage: $0 <project name> <feature branch>"
+    echo "usage: $0 <path to project workspace> <feature branch>"
     exit 1
 fi
 
+project=$(basename $(realpath "${workspace}")) &&
+echo "workspace ${workspace}" &&
+echo "feature_branch ${feature_branch}" &&
+echo "project ${project}" &&
+cd "${workspace}" &&
+ls app && echo 'workspace enthält Verzeichnis app; fahre fort...' || ( echo 'workspace enthält kein Verzeichnis app; bitte Argument 1 überprüfen.' && false ) &&
+echo 'Breche for git switch local ab zu Testzwicken' && false &&
 
 git switch local &&
-cd $(dirname $0)/../../../${project}_MAIN &&
+cd /../${project}_MAIN &&
 git rebase --onto common local "${feature_branch}" &&
 git switch common &&
 git merge --ff-only "${feature_branch}" &&
