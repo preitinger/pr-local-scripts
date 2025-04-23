f5f3d2014f04e715d77fea5a827ad9ccc280c676
preitinger <peter.reitinger@gmail.com>
2025-04-23 09:27:23 +0200

    common-submodule-add.sh bereit zum Test

--- script/common-submodule-add.sh
+++ script/common-submodule-add.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Wichtige Voraussetzungen für die Anwendung dieses Skripts:
+# branch local in project root (1. Argument)
+# Nichts gestaged, sonst wird das mi SUBMODULE ADD mit commitet.
+# Sauberer branch main in ${workspace}/../${project}_MAIN
+
 workspace="$1"
 submodule="$2"
 
@@ -9,14 +14,21 @@ then
     exit 1
 fi
 
+project=$(basename $(realpath ${workspace})) &&
+echo "project ${project}" &&
+echo "Skip rest while testing" && false &&
 cd "${workspace}" &&
 ls app && echo 'workspace enthält Verzeichnis app; fahre fort...' || ( echo 'workspace enthält kein Verzeichnis app; bitte Argument 1 überprüfen.' && false ) &&
-workspace=${PWD} &&
-mkdir -p "${workspace}/app/_lib/submodules" &&
-cd "${workspace}/app/_lib/submodules" &&
+mkdir -p "app/_lib/submodules" &&
+cd "app/_lib/submodules" &&
 pwd &&
 git submodule add --force ${submodule} &&
 git commit -m "SUBMODULE ADD ${submodule}" &&
+cd .. &&
+rsync -a --exclude='.*' "submodules" "../../../${project}_MAIN/app/_lib/" &&
+cd "../../../${project}_MAIN" &&
+git switch main && # zur Sicherheit
+
 
 echo 'The End.' ||
 echo 'ERROR'
